ba849536e7a8897aaa3aaeb35980765e3b2942b0
Jonas Pereira De Lima <jonasplima1@gmail.com>
2022-01-10 12:36:45 -0300

    Adicionando Seller e Salles

--- SalesMVC/Models/Department.cs
+++ SalesMVC/Models/Department.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace SalesMVC.Models
 {
@@ -6,5 +8,27 @@ namespace SalesMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void addSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double totalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.totalSales(initial, final));
+        }
     }
 }
